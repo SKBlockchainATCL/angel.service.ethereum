6597acace484292f3f8b5b48812f81338c3b39df
Oh Sangmoon <halfface@chollian.net>
2019-07-18 20:02:07 +0900

    Adding ServiceProgramFactory contract.

--- contracts/ServiceProgramFactory.sol
+++ contracts/ServiceProgramFactory.sol
@@ -4,4 +4,22 @@ import "./ServiceProgram.sol";
 
 contract ServiceProgramFactory{
 
+
+    mapping(address => ServiceProgram) programs;
+    address[] programAddresses;
+
+
+
+    function addServiceProgram(bytes32 _title, bytes8 _from, bytes8 _to) public{
+
+        ServiceProgram program = new ServiceProgram(_title, _from, _to);
+
+        programs[address(program)] = program;
+        programAddresses.push(address(program));
+    }
+
+    function countServicePrograms() public returns (uint){
+        return programAddresses.length;
+    }
+
 }
