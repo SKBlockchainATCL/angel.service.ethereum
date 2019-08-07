53fd91c4173d36d04d51db698d0bbcb492260ed6
Oh Sangmoon <halfface@chollian.net>
2019-08-07 21:23:41 +0900

    ...

--- contracts/ServiceProgramFactory.sol
+++ contracts/ServiceProgramFactory.sol
@@ -5,12 +5,8 @@ import "./ServiceProgram.sol";
 
 contract ServiceProgramFactory{
 
-
-    mapping(uint => address[]) programMapByState;
     mapping(address => ServiceProgram) programs;
     address[] programAddresses;
-
-
 
     function addServiceProgram(bytes32 _title, bytes8 _from, bytes8 _to) public{
 
@@ -19,15 +15,12 @@ contract ServiceProgramFactory{
         address addr = address(program);
         programs[addr] = program;
         programAddresses.push(addr);
-        programMapByState[uint(ServiceLib.ProgramStatus.Open)].push(addr);
-        
     }
 
     function countServicePrograms() public returns (uint){
         return programAddresses.length;
     }
-    
-    
+
     function updateServiceStatus(address addr, ServiceProgramState.Status status) public{
         
     }
